4ada4f6374084a5312b88cc8acd75ec03e859555
joxx0181 <62244562+joxx0181@users.noreply.github.com>
2020-11-05 16:17:33 +0100

    Add files via upload

---
+++ MyBanker/Cards/Credit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBanker
+{
+    class Credit : ATMcard
+    {
+        // parametrized constructor declaration!
+        public Credit (string type, string bal, string maxD, string rating, int credit) : base(type, bal)
+        {
+            // initialization!
+            MaxDay = maxD;
+            CreditRating = rating;
+            CreditLimitMdr = credit;
+        }
+
+        // auto implemented properties with get & set accessor  -  Encapsulation!
+        public string MaxDay { get; set; }
+        public string CreditRating { get; set; }
+        public int CreditLimitMdr { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\ncredit rating: " + CreditRating + "\nmax amount: " + MaxDay + "\ncredit limit (DKK) pr. mdr: " + CreditLimitMdr;
+        }
+    }
+}
---
+++ MyBanker/Cards/Debit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBanker
+{
+    class Debit : ATMcard
+    {
+        // parametrized constructor declaration!
+        public Debit (string type, string bal, string loca) : base(type, bal)
+        {
+            // initialization!
+            Location = loca;
+        }
+
+        // auto implemented properties with get & set accessor  -  Encapsulation!
+        public string Location { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\nthis card can be use: " + Location;
+        }
+    }
+}
