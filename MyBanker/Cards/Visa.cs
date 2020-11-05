50722ffd3b8266bb302eb4bdc3dc06ee93303820
joxx0181 <62244562+joxx0181@users.noreply.github.com>
2020-11-05 16:20:14 +0100

    Update Visa.cs

--- MyBanker/Cards/Visa.cs
+++ MyBanker/Cards/Visa.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace MyBanker
 {
-    class Visa : Mastercard
+    class Visa : Credit
     {
         // parametrized constructor declaration!
         public Visa (string type, string bal, string maxD, string rating, int credit, int overD) : base(type, bal, maxD, rating, credit)
         {
             // initialization!
-            overDraw = overD;
+            OverDraw = overD;
         }
 
         // auto implemented properties with get & set accessor  -  Encapsulation!
-        public int overDraw { get; set; }
+        public int OverDraw { get; set; }
 
         public override string ToString()
         {
-            return base.ToString() + "\nallowed overdraw: " + overDraw;
+            return base.ToString() + "\nallowed overdraw: " + OverDraw;
         }
     }
 }
