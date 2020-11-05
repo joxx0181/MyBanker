f2d584c84d5e7b35431019983718c7258d08c7b7
joxx0181 <62244562+joxx0181@users.noreply.github.com>
2020-11-05 16:16:13 +0100

    Update Maestro.cs

--- MyBanker/Cards/Maestro.cs
+++ MyBanker/Cards/Maestro.cs
@@ -4,21 +4,17 @@ using System.Text;
 
 namespace MyBanker
 {
-    class Maestro : ATMcard 
+    class Maestro : Debit
     { 
         // parametrized constructor declaration!
-        public Maestro (string type, string bal, string loca) : base (type, bal)
+        public Maestro (string type, string bal, string loca) : base (type, bal, loca)
         {
-        // initialization!
-        location = loca;
+ 
         }
-
-         // auto implemented properties with get & set accessor  -  Encapsulation!
-         public string location { get; set; }
 
          public override string ToString()
          {
-            return base.ToString() + "\nthis card can be use: " + location;
+            return base.ToString();
          }
     }
 }
